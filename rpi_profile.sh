6f06a2fc8a2a4aa8a00c7c8289d23bd4c094f117
Patrick Gavigan <pgavigan@gmail.com>
2021-06-29 16:00:42 -0400

    updated script

--- rpi_profile.sh
+++ rpi_profile.sh
@@ -1,7 +1,2 @@
-java -Dfile.encoding=Cp1252 -classpath /home/pi/Thesis/jason/libs/jason-2.6.jar:/home/pi/Thesis/jason/bin/ jason.infra.centralised.RunCentralisedMAS ${parameter}Test_${i}.mas2j & 
-
 javaPid=$!
-
-sleep 60s
-
-/home/pi/jprofiler12/bin/jpenable --noinput --offline --pid=${javaPid} --config=/home/pi/Thesis/jprofiler_config.xml
+/home/pi/jprofiler12/bin/jpenable --noinput --offline --pid=${javaPid} --config=/home/pi/savi_profiling/savi_profiling/jprofiler_config_ros.xml
