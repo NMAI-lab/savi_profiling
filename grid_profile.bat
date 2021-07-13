e7cd1876bc2bf0f81dafcc9dcb9955b819bb3557
Patrick Gavigan <pgavigan@gmail.com>
2021-07-13 15:13:51 -0400

    updated scripts

--- grid_profile.bat
+++ grid_profile.bat
@@ -4,7 +4,7 @@ for /F "TOKENS=1,2,*" %%a in ('tasklist /FI "IMAGENAME eq java.exe"') do set MyP
 
 timeout /t 10 /nobreak > NUL
 
-"C:\Program Files\jprofiler12\bin\jpenable" --noinput --offline --pid=%MyPID% --config="D:\Local Documents\Github\savi_profiling\jprofiler_config.xml"
+"C:\Program Files\jprofiler12\bin\jpenable" --noinput --offline --pid=%MyPID% --config="D:\Local Documents\Github\savi_profiling\jprofiler_config_deliberate.xml"
 
 timeout /t 10 /nobreak > NUL
 
--- ros_profile.bat
+++ ros_profile.bat
@@ -1,6 +1,6 @@
 for /F "TOKENS=1,2,*" %%a in ('tasklist /FI "IMAGENAME eq java.exe"') do set MyPID=%%b
 
-"C:\Program Files\jprofiler12\bin\jpenable" --noinput --offline --pid=%MyPID% --config="D:\Local Documents\Github\savi_profiling\jprofiler_config_ros.xml"
+"C:\Program Files\jprofiler12\bin\jpenable" --noinput --offline --pid=%MyPID% --config="D:\Local Documents\Github\savi_profiling\jprofiler_config_ros_deliberate.xml"
 
 timeout /t 30 /nobreak > NUL
 
