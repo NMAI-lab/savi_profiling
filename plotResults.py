29241842ffe5c2604d1c1a3eb1eedee3c3b1d096
Patrick Gavigan <pgavigan@gmail.com>
2021-07-14 21:20:24 -0400

    starting plot tool

---
+++ plotResults.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 14 11:02:30 2021
+
+@author: Patrick
+"""
+
+def analyze():
+    directory = 'D:/Local Documents/Github/savi_profiling/results'
+    
+    
+    
+if __name__ == '__main__':
+    analyze()
