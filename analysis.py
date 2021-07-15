2c397b7c2732582352d24af612fce7caa7a23f2a
Patrick Gavigan <pgavigan@gmail.com>
2021-07-14 21:20:16 -0400

    analysis tool done

--- analysis.py
+++ analysis.py
@@ -41,13 +41,18 @@ def profileXmlToCsv(inputDir):
                            'applyFindOp',
                            'selectAction',
                            'applyExecInt',
-                           'deliberate']
+                           'deliberate',
+                           'sense',
+                           'act']
 
-    outputPath = inputDir + '/Cumulative_Data'
-    outputName = outputPath + '/Cumulative.csv'
+    outputPath = inputDir + 'Cumulative_Data/'
+    outputName = outputPath + 'Cumulative.csv'
     if (not os.path.exists(outputPath)):
         os.mkdir(outputPath)
+    if os.path.exists(outputName):
+        os.remove(outputName)
 
+    # Get the names of all the XML files
     xmlFiles = list()
     for file in os.listdir(inputDir):
         if file.endswith(".xml"):
@@ -55,78 +60,40 @@ def profileXmlToCsv(inputDir):
 
     line = 1
     df = pd.DataFrame(columns = interestedFunctions)
-    
-    run = 1
-    while (True):
-        printProgressBar (run, len(xmlFiles))
-        runFile_p = inputDir + '/snapshot_' + str(run - 1) + '.xml'
-        runFile = inputDir + '/snapshot_' + str(run) + '.xml'
-    
-        outputName = outputPath + '/Cumulative.csv'
-        if (not os.path.exists(runFile)):
-            print(runFile + " doesn't exist, aborting")
-            break
-    
+
+    for file in xmlFiles:
+        printProgressBar (line, len(xmlFiles))
+        runFile = inputDir + file
         p = etree.XMLParser(huge_tree = True)
-        doc_p = etree.parse(runFile_p, parser = p)
         doc = etree.parse(runFile, parser = p)
     
-        rcNode_p = doc_p.find("//node[@methodName=\"reasoningCycle\"]")
-        rcNode = doc.find("//node[@methodName=\"reasoningCycle\"]")
-        
-        deliberateNode_p = doc_p.find("//node[@methodName=\"deliberate\"]")
-        deliberateNode = doc.find("//node[@methodName=\"deliberate\"]")
-        
-        if ((rcNode_p == None) and (rcNode == None)):
-            if ((deliberateNode_p == None) and (deliberateNode == None)):
-                run += 1
-                continue
-    
-        data = []
-        for x in interestedFunctions:
-    
-            if (x == "reasoningCycle"):
-                tempTime_p = int(rcNode_p.get("time"))
-                tempTime = int(rcNode.get("time"))
-            else:    
-    
-                temp_p = rcNode_p.find(".//node[@methodName='" + x + "']")
-                temp = rcNode.find(".//node[@methodName='" + x + "']")
-                if (temp == None):
-                    tempTime = 0
-                else:
-                    tempTime = int(temp.get("time"))
-    
-                if (temp_p == None):
-                    tempTime_p = 0
-                else:
-                    tempTime_p = int(temp_p.get("time"))
-    
-    
-            timeDiff = tempTime - tempTime_p
-    
-            data.append(str(timeDiff))
-    
-    
-        df.loc[line] = data
+        row = []    
+        for functionName in interestedFunctions:
+            functionData = doc.find(".//node[@methodName='" + functionName + "']")
+            if functionData == None:
+                row.append('-1')
+            else:
+                row.append(str(functionData.get("time")))
+         
+        df.loc[line] = row
         line += 1
-        run += 1
+        
     
-        df.to_csv(outputName, header = True)
+    df.to_csv(outputName, header = True)
     
     
 def analyze():
-    directoryList = ["Y:/School/PhD/ProfileResults/car/deliberate/profiles",
-                     "Y:/School/PhD/ProfileResults/car/reassoningCycle/profiles",
-                     "Y:/School/PhD/ProfileResults/gridRosPC/decide/profiles",
-                     "Y:/School/PhD/ProfileResults/gridRosPC/reasoningCycle/profiles",
-                     "Y:/School/PhD/ProfileResults/gridSynched/reasoningCycle/profiles",
-                     "Y:/School/PhD/ProfileResults/gridSynched/synchGridDeliberate/profiles",
-                     "Y:/School/PhD/ProfileResults/mail/deliberate/profiles",
-                     "Y:/School/PhD/ProfileResults/mail/reasoningCycle/profiles",
-                     "Y:/School/PhD/ProfileResults/rosGridRpi/deliberate/profiles",
-                     "Y:/School/PhD/ProfileResults/rosGridRpi/reasoningCycle/profiles"]
-  
+    directoryList = ["Y:/School/PhD/ProfileResults/car/deliberate/profiles/",
+                     "Y:/School/PhD/ProfileResults/car/reassoningCycle/profiles/",
+                     "Y:/School/PhD/ProfileResults/gridRosPC/decide/profiles/",
+                     "Y:/School/PhD/ProfileResults/gridRosPC/reasoningCycle/profiles/",
+                     "Y:/School/PhD/ProfileResults/gridSynched/reasoningCycle/profiles/",
+                     "Y:/School/PhD/ProfileResults/gridSynched/synchGridDeliberate/profiles/",
+                     "Y:/School/PhD/ProfileResults/mail/deliberate/profiles/",
+                     "Y:/School/PhD/ProfileResults/mail/reasoningCycle/profiles/",
+                     "Y:/School/PhD/ProfileResults/rosGridRpi/deliberate/profiles/",
+                     "Y:/School/PhD/ProfileResults/rosGridRpi/reasoningCycle/profiles/"]
+    
     for i in range(len(directoryList)):
         print("Convertig " + str(i) + " of " + str(len(directoryList)))
         profileXmlToCsv(directoryList[i])
